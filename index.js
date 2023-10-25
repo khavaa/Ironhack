1b1b662ba3a4c7d0184c3a96a1caa361ac675c1f
khavaa <khavaom@gmail.com>
2023-10-25 12:27:30 +0200

    Initial commit

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-const chalk = require('chalk');
-
 const colleagues = ['Melissa', 'Milhan', 'Théo', 'Antoine'];
+const colors = ['red', 'green', 'blue', 'orange', 'blue', 'yellow','white'];
 
 colleagues.forEach((colleague, index) => {
-  const coloredName = chalk.keyword(getRandomColor())(colleague);
-  console.log(`Colleague ${index + 1}: ${coloredName}`);
+  const randomColor = colors[Math.floor(Math.random() * colors.length)];
+  console.log(`Colleague ${index + 1}: \x1b[38;2;${randomColor}m${colleague}\x1b[0m`);
 });
-
-function getRandomColor() {
-  const colors = ['red', 'green', 'blue', 'yellow', 'white'];
-  return colors[Math.floor(Math.random() * colors.length)];
-}
